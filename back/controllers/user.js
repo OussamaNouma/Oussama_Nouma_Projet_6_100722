e718c3519f11cd465b5c46258ca16767da9107fa
Samsky-png <oussama.nouma1@gmail.com>
2022-07-27 20:47:30 +0200

    Feat(controllers) adding controller for both users and sauces

---
+++ back/controllers/user.js
@@ -0,0 +1,44 @@
+const bcrypt = require("bcrypt");
+const { json } = require("express");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+exports.signup = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    const user = new User({
+      email: req.body.email,
+      password: hash,
+    });
+    user
+      .save()
+      .then(() => res.status(201).json({ message: "user created" }))
+      .catch((error) => res.status(500).json({ error }));
+  });
+};
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ message: "Paire indentifiant/mot de passe incorrecte" });
+      }
+      bcrypt.compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({
+              message: "Paire indentifiant/mot de passe incorrecte",
+            });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign({ userId: user._id }, 
+              process.env.SECRET_KEY, 
+              {expiresIn: "24h"}
+              )
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+};
